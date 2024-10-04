426eae80e23b621521086d15e6164d2b87441015
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-10-04 07:01:58 +0200

    tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -5,8 +5,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/styles/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function App() {
+ 
   return (
     <html lang="en">
       <head>
